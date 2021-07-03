dc03c99356b20cc0c83acf189b5bb030d803e563
awx <callmeoniichandesu@gmail.com>
2021-07-03 17:57:14 +0700

    Initial app realese

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import CSS from 'csstype'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const textAreaStyle: CSS.Properties = {
+  minHeight: '15em',
+  minWidth: '15em',
+  backgroundColor: 'black',
+  color: 'white',
+  outline: 'none',
+  margin: '10px'
 }
 
-export default App;
+const mainDiv: CSS.Properties = {
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+const button: CSS.Properties = {
+  color: '#0099CC',
+  backgroundColor: 'transparent',
+  border: '2px solid #0099CC',
+  borderRadius: '5px',
+  textTransform: 'uppercase',
+  padding: '5px 10px',
+  margin: '20px 0',
+}
+function parser(text: string): string {
+  try {
+    try {
+      const isJson = JSON.parse(text)
+      if (isJson) return JSON.stringify(isJson, null, 2)
+    } catch (error) {
+      // basically do nothing
+    }
+    const result: any = {}
+    text.split('\n').forEach((line) => {
+      if (!line) return
+      const [key, ...splitValue] = line.split(':')
+      const value = splitValue.join(':')
+      result[key.trim()] = value.trim()
+    })
+
+    return JSON.stringify(result, null, 2)
+  } catch (e) {
+    return 'invalid input'
+  }
+}
+
+function copy(text: string): void {
+  let textArea = document.createElement('textarea')
+  textArea.value = text
+
+  textArea.style.top = '0'
+  textArea.style.left = '0'
+  textArea.style.position = 'fixed'
+
+  document.body.appendChild(textArea)
+  textArea.focus()
+  textArea.select()
+
+  try {
+    const successful = document.execCommand('copy')
+    const msg = successful ? 'successful' : 'unsuccessful'
+    document.body.removeChild(textArea)
+    alert(msg)
+  } catch (err) {
+    console.error(err)
+    alert('unable to copy')
+  }
+}
+
+function App() {
+  const [text, setText] = useState('{}')
+
+  return (
+    <div style={mainDiv}>
+      <div>
+        <div>
+          <textarea
+            style={textAreaStyle}
+            onChange={(event) => setText(parser(event.target.value))}
+          ></textarea>
+        </div>
+        <div>
+          <textarea id="result" value={text} style={textAreaStyle}></textarea>
+        </div>
+        <div style={mainDiv}>
+          <button style={button} onClick={() => copy(text)}>Copy Result</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
